2ca444eb409d5b2974c90752bd52d40fcec5142d
sameer <sameerswain634@gmail.com>
2025-05-13 00:28:49 +0530

    msg

--- src/main/java/com/sameer/ExampleProjectApplication.java
+++ src/main/java/com/sameer/ExampleProjectApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExampleProjectApplication {
-	int x=100;
-	int y=200;
-	boolean b=true;
-	
+
 	public static void main(String[] args) {
 	
 	int x=100;
