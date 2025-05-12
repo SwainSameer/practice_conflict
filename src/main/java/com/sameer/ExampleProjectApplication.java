0ac448ab757eac5e0a04a8ee2318b31011b05d7b
SwainSameer <sameerswain634@gmail.com>
2025-05-13 00:27:45 +0530

    Update ExampleProjectApplication.java
    add

--- src/main/java/com/sameer/ExampleProjectApplication.java
+++ src/main/java/com/sameer/ExampleProjectApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExampleProjectApplication {
-	int x=100;
-	int y=200;
-	boolean b=true;
-	
 	public static void main(String[] args) {
+		int x=100;
+		int y=200;
+		boolean b =false;
 		SpringApplication.run(ExampleProjectApplication.class, args);
 	}
 
